03f746cdf7e86270474d0b28dcf164556179b7ac
Pierre-Louis BRUN <pilouis.brun@gmail.com>
2025-01-13 18:38:06 +0100

    3 more questions

--- src/questions.py
+++ src/questions.py
@@ -13,5 +13,21 @@ questions = [
         "question": "Who wrote 'Romeo and Juliet'?",
         "options": ["Charles Dickens", "William Shakespeare", "Jane Austen", "Mark Twain"],
         "correct": 1
+    },
+    {
+        "question": "Which year did the Titanic sink?",
+        "options": ["1905", "1912", "1920", "1935"],
+        "correct": 1
+    },
+    {
+        "question": "What is the largest ocean on Earth?",
+        "options": ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+        "correct": 3
+    },
+    {
+        "question": "Who is the author of '1984'?",
+        "options": ["George Orwell", "Aldous Huxley", "Ray Bradbury", "J.D. Salinger"],
+        "correct": 0
     }
+
 ]
